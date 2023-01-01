deecf8b89f32b0f117c91c14153b35c7c6cc2304
leonardo.gabriel <leonardo.gabriel@take.net>
2023-01-01 16:10:42 -0300

    Added spaces

--- lib/themes/fields/grx_field_styles.theme.dart
+++ lib/themes/fields/grx_field_styles.theme.dart
@@ -8,10 +8,13 @@ import '../typography/styles/grx_headline_small_text.style.dart';
 abstract class GrxFieldStyles {
   static const inputTextStyle =
       GrxCaptionLargeTextStyle(color: GrxColors.cff7892b7);
+
   static const inputHintTextStyle =
       GrxCaptionLargeTextStyle(color: GrxColors.cff7892b7);
+
   static const labelTextStyle =
       GrxHeadlineSmallTextStyle(color: GrxColors.cff2e2e2e);
+
   static const inputErrorTextStyle =
       GrxCaptionTextStyle(color: GrxColors.cfffc5858);
 
