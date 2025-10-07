b50dcf5b98787f2fbba761f189d3fbd9dc6f0803
Leonardo Gabriel <leonardo.rgabriel@outlook.com>
2025-10-07 14:51:22 -0300

    fix: lint errors

--- lib/widgets/typography/grx_display_large_text.widget.dart
+++ lib/widgets/typography/grx_display_large_text.widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../enums/grx_text_transform.enum.dart';
 import '../../themes/colors/grx_colors.dart';
-import '../../themes/typography/styles/grx_Display_large_text.style.dart';
+import '../../themes/typography/styles/grx_display_large_text.style.dart';
 import 'grx_text.widget.dart';
 
 /// A Design System's [Text] primarily used by large titles
