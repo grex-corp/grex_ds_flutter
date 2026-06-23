0a95e5da0f5f7c3de9625257a12412fb387ff237
Leonardo Gabriel <leonardo.rgabriel@outlook.com>
2026-06-23 15:29:32 -0300

    fix: update regex patterns to include alphanumeric characters and improve checkbox opacity handling
    - Modified regex patterns in GrxRegexUtils to allow numbers in single and full name avatars.
    - Updated GrxCheckboxListTile to use withValues for opacity calculation based on enabled state.

--- lib/utils/grx_regex.util.dart
+++ lib/utils/grx_regex.util.dart
@@ -1,6 +1,6 @@
 abstract class GrxRegexUtils {
-  static const singleNameAvatarRgx = r'[A-Za-z]';
-  static const fullNameAvatarRgx = r'\b[A-Za-z]';
+  static const singleNameAvatarRgx = r'[A-Za-z0-9]';
+  static const fullNameAvatarRgx = r'\b[A-Za-z0-9]';
   static const emailRgx =
       r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
   static const numericOnlyRgx = r'[^0-9]';
