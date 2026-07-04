1b3d503f1fd59e79a1890e1b02159322836c9432
Leonardo Gabriel <leonardo.rgabriel@outlook.com>
2026-07-03 23:20:20 -0300

    feat: add loading state to GrxChip with shimmer effect
    - Introduced an `isLoading` property to GrxChip to handle loading states.
    - Implemented a shimmer effect in the GrxChip widget when loading, enhancing user experience.
    - Updated the build method to conditionally render the shimmer or the label based on the loading state.
    - Added a private `_buildShimmer` method to encapsulate shimmer rendering logic.

--- lib/widgets/grx_chip.widget.dart
+++ lib/widgets/grx_chip.widget.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import '../enums/grx_chip_type.enum.dart';
 import '../themes/colors/grx_colors.dart';
+import '../themes/radius/grx_radius.dart';
 import '../themes/spacing/grx_spacing.dart';
+import '../themes/typography/styles/grx_label_text.style.dart';
+import 'grx_shimmer.widget.dart';
 import 'typography/grx_label_text.widget.dart';
 
 class GrxChip extends StatelessWidget {
-  const GrxChip({super.key, required this.type, this.label, this.builder})
-    : assert((type != GrxChipType.custom && label != null) || (type == GrxChipType.custom && builder != null));
+  const GrxChip({
+    super.key,
+    required this.type,
+    this.label,
+    this.builder,
+    this.isLoading = false,
+  }) : assert(
+         (type != GrxChipType.custom && label != null) ||
+             (type == GrxChipType.custom && builder != null),
+       );
 
   final GrxChipType type;
   final String? label;
   final Widget Function(BuildContext context)? builder;
+  final bool isLoading;
 
   Color get backgroundColor => switch (type) {
     GrxChipType.primary => GrxColors.primary,
@@ -40,6 +53,10 @@ class GrxChip extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    if (isLoading && label != null) {
+      return _buildShimmer(context);
+    }
+
     return Material(
       shape: StadiumBorder(side: BorderSide(color: borderColor)),
       color: backgroundColor,
@@ -47,9 +64,40 @@ class GrxChip extends StatelessWidget {
           type == GrxChipType.custom && builder != null
               ? builder!(context)
               : Padding(
-                padding: const EdgeInsets.symmetric(horizontal: GrxSpacing.sm, vertical: GrxSpacing.xxs),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: GrxSpacing.sm,
+                  vertical: GrxSpacing.xxs,
+                ),
                 child: GrxLabelText(label, color: foregroundColor),
               ),
+    );
+  }
+
+  Widget _buildShimmer(BuildContext context) {
+    const padding = EdgeInsets.symmetric(
+      horizontal: GrxSpacing.sm,
+      vertical: GrxSpacing.xxs,
+    );
+
+    final style = GrxLabelTextStyle(color: foregroundColor);
+    final renderParagraph = RenderParagraph(
+      TextSpan(text: label, style: style),
+      textDirection: TextDirection.ltr,
+      maxLines: 1,
+    );
+
+    final size = MediaQuery.sizeOf(context);
+    renderParagraph.layout(
+      BoxConstraints(maxHeight: size.height, maxWidth: size.width),
+    );
+
+    final textHeight = renderParagraph.getMinIntrinsicHeight(style.fontSize!);
+    final textWidth = renderParagraph.getMinIntrinsicWidth(style.fontSize!);
+
+    return GrxShimmer(
+      height: textHeight + padding.vertical,
+      width: textWidth + padding.horizontal,
+      borderRadius: GrxRadius.round,
     );
   }
 }
