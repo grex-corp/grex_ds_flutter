893b55fbd84cda82fc855273e1c2eb780f98099d
Leonardo Gabriel <leonardo.rgabriel@outlook.com>
2026-07-06 18:47:50 -0300

    refactor: update GrxFilterButton to use GrxRoundedButton and enhance filter state handling
    - Changed GrxFilterButton to extend GrxRoundedButton for improved styling.
    - Introduced a new `hasFiltersApplied` parameter to dynamically adjust button colors and text styles based on filter state.
    - Updated color and text style logic to enhance visual feedback when filters are applied.

--- lib/widgets/buttons/grx_filter_button.widget.dart
+++ lib/widgets/buttons/grx_filter_button.widget.dart
@@ -4,9 +4,10 @@ import '../../enums/grx_shape.enum.dart';
 import '../../themes/colors/grx_colors.dart';
 import '../../themes/icons/grx_icons.dart';
 import '../../themes/typography/styles/grx_body_text.style.dart';
-import 'grx_secondary_button.widget.dart';
+import '../../themes/typography/styles/grx_title_small_text.style.dart';
+import 'grx_rounded_button.widget.dart';
 
-class GrxFilterButton extends GrxSecondaryButton {
+class GrxFilterButton extends GrxRoundedButton {
   GrxFilterButton({
     super.key,
     required super.text,
@@ -14,13 +15,20 @@ class GrxFilterButton extends GrxSecondaryButton {
     super.margin,
     super.enabled,
     super.padding,
+    bool hasFiltersApplied = false,
   }) : super(
          icon: GrxIcons.filter_list,
          mainAxisSize: MainAxisSize.min,
          shape: GrxShape.circle,
-         borderColor: GrxColors.neutrals.shade50,
-         iconColor: GrxColors.primary,
-         foregroundColor: GrxColors.neutrals.shade1000,
-         textStyle: GrxBodyTextStyle(),
+         borderColor:
+             hasFiltersApplied ? GrxColors.primary : GrxColors.neutrals.shade50,
+         iconColor: hasFiltersApplied ? GrxColors.neutrals : GrxColors.primary,
+         foregroundColor:
+             hasFiltersApplied
+                 ? GrxColors.neutrals
+                 : GrxColors.neutrals.shade1000,
+         backgroundColor: hasFiltersApplied ? GrxColors.primary : null,
+         textStyle:
+             hasFiltersApplied ? GrxTitleSmallTextStyle() : GrxBodyTextStyle(),
        );
 }
